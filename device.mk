cf41cccb743247cf7c8da1d2b403015a069f0372
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-12 22:41:36 +0800

    Apollo: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992

--- device.mk
+++ device.mk
@@ -279,6 +279,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayCommon \
+    DeviceAsWebcamResCommon \
     FrameworkResOverlayCommon \
     LineageSDKOverlayCommon \
     LineageSettingsOverlayCommon \
