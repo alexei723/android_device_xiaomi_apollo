c7e2a94afecbc8f98a76a8dcf7598586ee41fc6f
Murtaza Hussain <160129300+MurtazaKolachi@users.noreply.github.com>
2026-06-26 03:28:14 +0500

    Revert "Apollo: Utilize task_profiles.json + cgroups.json"
    This reverts commit 9c510b29257beab067c8480b0448aee3df67d47b.

--- device.mk
+++ device.mk
@@ -343,8 +343,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
-    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
 # Public libraries
